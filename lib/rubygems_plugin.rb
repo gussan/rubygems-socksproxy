3fd35cad979f95b7e64217ab74aa034d11978f96
gussan <egussan@gmail.com>
2013-05-20 23:02:52 +0900

    Support bundler

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,6 +1,1 @@
-if ENV['SOCKS_PROXY']
-  require 'socksify'
-  host, port = ENV['SOCKS_PROXY'].split(':')
-  TCPSocket::socks_server = host || 'localhost'
-  TCPSocket::socks_port = port.to_i || 1080
-end
+require 'rubygems/socksproxy'
